a8fe7a9c044991b8dfd744fda29c0bde626c6f4e
nicolasecuello <nicolas.emiliano@hotmail.com.ar>
2021-10-31 17:46:58 -0300

    Agregado de calculo de perdida de metal

--- Calculosdepozos.py
+++ Calculosdepozos.py
@@ -1,33 +1,50 @@
 #import numpy as np
 import pandas as pd
-import plotly as px
 
-#read_csv('C:\Users\nicol\OneDrive\Escritorio\Automatizacion de tareas con PY')
-
-#archivo = open('datos.xlsx',encoding = 'utf-8')
-
-#========== DATOS DE ENTRADA =============
+#------------- DATOS DE ENTRADA -------------
 
 ri = 2
 re = 3
+n = 4           # CANTIDAD DE DEDOS
 
+#--------------------------------------------
+
+radios_cuad = re**2-ri**2
 di = ri*2
 de = re*2
-
 e = re-ri
 
-#==========================================
-
+#------------------- LECTURA DE DATOS ----------------------
 
 df = pd.read_excel('datos.xlsx')
 df2 = df.drop(columns=['Profundidad'])
+print('Carga de datos OK')
 
+#---------------RECORRE DATAFRAME Y CORRIGE VALORES FUERA DEL RADIO INTERNO Y EXTERNO----
 
-df['Maxima_Prof'] = [re     if df2.max(axis=1)[i]>re    else df2.max(axis=1)[i]     for i in range(len(df['Profundidad'])) ]
-df['Minima_Prof'] = [ri     if df2.min(axis=1)[i]<ri    else df2.min(axis=1)[i]     for i in range(len(df['Profundidad'])) ]
+for i in range(0,len(df2)):
+   for j in range(0,n):
+       if df2.loc[i,:][j]<ri: df2.loc[i,:][j] = ri
+       if df2.loc[i,:][j]>re: df2.loc[i,:][j] = re
+       else: df2.loc[i,:][j] = df2.loc[i,:][j]
+         
+#---------------CALCULOS DE MAXIMAS Y MINIMAS PENETRACIONES POR FILA------------------
 
-df
+df2['Maxima_Prof'] = [re     if df2.max(axis=1)[i]>re    else df2.max(axis=1)[i]     for i in range(len(df['Profundidad'])) ]
+df2['Minima_Prof'] = [ri     if df2.min(axis=1)[i]<ri    else df2.min(axis=1)[i]     for i in range(len(df['Profundidad'])) ]
 
+print('Calulo de máximas y mínimas penetraciones de defectos OK')
 
+#---------------CALCULO DE PERDIDA DE AREA EN FUNCION DE LOS DEDOS----------------------
 
-
+lista = []
+for i in range(0,len(df2)):
+   a1 = 0
+   for j in range(0,n):
+       a = re**2 - df2.loc[i,:][j]**2
+       a1 = a + a1
+   PM = 100 - 100/(n*radios_cuad) * a1    
+   lista.append(PM.round(2))
+   
+df2['Perdida_de_metal'] = lista
+   
